fd5b192ae14aaa5b38ea95eaf9928c3d166794b0
g <f>
2019-11-26 14:04:50 +0200

    ReadText

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,14 +12,24 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        List<string> text { get; set; }
         public Form1()
         {
             InitializeComponent();
+            text = new List<string>();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text files(*.txt)|*.txt";
+            if (ofd.ShowDialog() == DialogResult.Cancel)
+                return;
+            
+            string filename = ofd.FileName;
+           
+            text = System.IO.File.ReadAllLines(filename).ToList();
+            
         }
     }
 }
